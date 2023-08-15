b18e016f9297c8014b79dc09e439af3bc9de7f29
BryanCh0ng <90763256+BryanCh0ng@users.noreply.github.com>
2023-08-15 14:04:13 +0800

    Update allClasses.py

--- backend/allClasses.py
+++ backend/allClasses.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -16,24 +17,445 @@ db = SQLAlchemy(app)
 
 CORS(app, supports_credentials=True)
 
-################## Staff Class Creation ##################
-class Staff(db.Model):
-    __tablename__ = 'staff'
+################## User Class Creation ##################
 
-    staff_ID = db.Column(db.Integer, nullable=False, primary_key=True)
-    staff_FName = db.Column(db.String(50), nullable=False)
-    staff_LName = db.Column(db.String(50), nullable=False)
-    dept = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    account = db.Column(db.Integer, db.ForeignKey('accounts.account_Name'), nullable=False)
+class User(db.model):
+    __tablename__ = 'user'
 
-    def __init__(self, staff_ID, staff_FName, staff_LName, dept, email, account):
-        self.staff_ID = staff_ID
-        self.staff_FName = staff_FName
-        self.staff_LName = staff_LName
-        self.dept = dept        
-        self.email = email
-        self.account = account   
+    user_ID = db.column(db.Integer, nullable=False, primary_key=True)
+    user_Name = db.Column(db.String(255), nullable=False)
+    user_Email = db.Column(db.String(155), nullable=False)
+    user_Password = db.Column(db.String(50), nullable=False)
+    role_Name = db.Column(db.String(20), nullable=False)
+
+    def __init__(self, user_ID, user_Name, user_Email, user_Password, role_Name):
+        self.user_ID = user_ID
+        self.user_Name = user_Name
+        self.user_Email = user_Email
+        self.user_Password = user_Password        
+        self.role_Name = role_Name
+    
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+        
+
+################## External User Class Creation ##################
+
+class ExternalUser(db.model):
+    __tablename__ = 'externaluser'
+
+    external_ID = db.column(db.Integer, nullable=False, primary_key=True)
+    user_ID = db.Column(db.Integer, db.ForeignKey('user.user_ID'),  nullable=False)
+    organisation_Name = db.Column(db.String(255), nullable=False)
+    is_Alumni = db.Column(db.boolean, nullable=False)
+
+    def __init__(self, external_ID, user_ID, organisation_Name, is_Alumni):
+        self.external_ID = external_ID
+        self.user_ID = user_ID
+        self.organisation_Name = organisation_Name
+        self.is_Alumni = is_Alumni        
+    
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+        
+################## Course Category Class Creation ##################
+
+class CourseCategory(db.model):
+    __tablename__ = 'coursecategory'
+
+    coursecat_ID = db.column(db.Integer, nullable=False, primary_key=True)
+    coursecat_Name = db.Column(db.String(10), nullable=False)
+
+
+    def __init__(self, coursecat_ID, coursecat_Name):
+        self.coursecat_ID = coursecat_ID
+        self.coursecat_Name = coursecat_Name
+
+    
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+        
+
+
+################## Course Class Creation ##################
+class Course(db.Model):
+    __tablename__ = 'course'
+
+    course_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    cousre_Name = db.Column(db.String(255), nullable=False)
+    course_Desc = db.Column(db.String(800), nullable=False)
+    coursecat_ID = db.Column(db.Integer, db.ForeignKey('coursecategory.coursecat_ID'), nullable=False)
+
+
+    def __init__(self, course_ID, cousre_Name, course_Desc, coursecat_ID):
+        self.course_ID = course_ID
+        self.cousre_Name = cousre_Name
+        self.course_Desc = course_Desc
+        self.coursecat_ID = coursecat_ID        
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+################## Propose Course Class Creation ##################
+class ProposedCourse(db.Model):
+    __tablename__ = 'proposedcourse'
+
+    pcourse_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    submitted_By = db.Column(db.Integer,db.ForeignKey('user.user_ID'), nullable=False)
+    action_Done_By = db.Column(db.Integer,db.ForeignKey('user.user_ID'), nullable=False)
+    course_ID = db.Column(db.Integer,db.ForeignKey('course.course_ID'), nullable=False)
+    pcourse_Status = db.Column(db.String(20), nullable=False)
+    reason = db.Column(db.String(255), nullable=False)
+
+
+
+    def __init__(self, pcourse_ID, submitted_By, action_Done_By, course_ID, pcourse_Status, reason ):
+        self.pcourse_ID = pcourse_ID
+        self.submitted_By = submitted_By
+        self.course_ID = course_ID
+        self.pcourse_Status = pcourse_Status
+        self.action_Done_By = action_Done_By
+        self.reason = reason        
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+################## Vote Course Class Creation ##################
+class VoteCourse(db.Model):
+    __tablename__ = 'votecourse'
+
+    vote_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    course_ID = db.Column(db.Integer,db.ForeignKey('course.course_ID'), nullable=False)
+    vote_Status = db.Column(db.String(15), nullable=False)
+
+
+
+
+    def __init__(self, vote_ID, course_ID, vote_Status ):
+        self.vote_ID = vote_ID
+        self.course_ID = course_ID
+        self.vote_Status = vote_Status
+
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+
+################## Interest Class Creation ##################
+class Interest(db.Model):
+    __tablename__ = 'interest'
+
+    interest_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    vote_ID = db.Column(db.Integer,db.ForeignKey('votecourse.vote_ID'), nullable=False)
+    user_ID = db.Column(db.Integer,db.ForeignKey('user.user_ID'),  nullable=False)
+
+
+
+
+    def __init__(self, interest_ID, vote_ID, user_ID ):
+        self.interest_ID = interest_ID
+        self.vote_ID = vote_ID
+        self.user_ID = user_ID
+
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+################## Feedback Template Class Creation ##################
+class FeedbackTemplate(db.Model):
+    __tablename__ = 'feedbacktemplate'
+
+    template_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    template_Name = db.Column(db.String(255), nullable=False)
+    # created_on = db.Column(db.DateTime, default=datetime.now ,  nullable=False)
+    created_On = db.Column(db.Date,  nullable=False)
+
+
+
+    def __init__(self, template_ID, template_Name, created_On ):
+        self.template_ID = template_ID
+        self.template_Name = template_Name
+        self.created_On = created_On
+
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+##################  Template Attribute Class Creation ##################
+class TemplateAttribute(db.Model):
+    __tablename__ = 'templateattribute'
+
+    template_Attribute_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    question = db.Column(db.String(255), nullable=False)
+    input_Type = db.Column(db.String(255),  nullable=False)
+    template_ID = db.Column(db.Integer, db.ForeignKey('feedbacktemplate.template_ID'),  nullable=False) 
+
+
+
+    def __init__(self, template_Attribute_ID, question, input_Type, template_ID ):
+        self.template_Attribute_ID = template_Attribute_ID
+        self.question = question
+        self.input_Type = input_Type
+        self.template_ID = template_ID
+
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+##################  Likert Scale Class Creation ##################
+class LikeryScale(db.Model):
+    __tablename__ = 'likertscale'
+
+    likert_Scale_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    template_Attribute_ID = db.Column(db.Integer,db.ForeignKey('templateattribute.template_Attribute_ID'),  nullable=False)
+    position = db.Column(db.Integer,  nullable=False)
+    textlabel = db.Column(db.String(255), nullable=False) 
+
+
+
+    def __init__(self, likert_Scale_ID, template_Attribute_ID, position, textlabel ):
+        self.likert_Scale_ID = likert_Scale_ID
+        self.template_Attribute_ID = template_Attribute_ID
+        self.position = position
+        self.textlabel = textlabel
+
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+##################  Feedback Class Creation ##################
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+
+    feedback_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    feedback_Template_ID = db.Column(db.Integer,  nullable=False)
+    submitted_By = db.Column(db.Integer,  nullable=False)
+    template_Attribute_ID = db.Column(db.Integer ,db.ForeignKey('templateattribute.template_attribute_ID'), nullable=False) 
+    answer = db.Column(db.String(255), nullable=False) 
+    course_ID = db.Column(db.Integer, db.ForeignKey('course.course_ID'), nullable=False) 
+
+
+
+    def __init__(self, feedback_ID, feedback_Template_ID, submitted_By, template_Attribute_ID, answer, course_ID):
+        self.feedback_ID = feedback_ID
+        self.feedback_Template_ID = feedback_Template_ID
+        self.submitted_By = submitted_By
+        self.template_Attribute_ID = template_Attribute_ID
+        self.answer = answer
+        self.course_ID = course_ID
+
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+################## Run Course Class Creation ##################
+class RunCourse(db.Model):
+    __tablename__ = 'runcourse'
+
+    rcourse_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    run_Startdate = db.Column(db.Date, nullable=False)
+    run_Enddate = db.Column(db.Date, nullable=False)
+    run_Starttime = db.Column(db.Time, nullable=False)
+    run_Endtime = db.Column(db.Time, nullable=False)
+    instructor_ID = db.Column(db.Integer, db.ForeignKey('user.user_ID'), nullable=False)
+    course_Format = db.Column(db.String(20), nullable=False)
+    course_Venue = db.Column(db.String(255), nullable=False)
+    runcourse_Status = db.Column(db.String(15), nullable=False)
+    course_Size = db.Column(db.Integer, nullable=False)
+    course_Minsize = db.Column(db.Integer, nullable=False)
+    course_Fee = db.Column(db.Integer, nullable=False)
+    class_Duration = db.Column(db.Integer, nullable=False)
+    reg_Startdate = db.Column(db.Date, nullable=False)
+    reg_Enddate = db.Column(db.Date, nullable=False)
+    reg_Starttime = db.Column(db.Time, nullable=False)
+    reg_Endtime = db.Column(db.Time, nullable=False)
+    template_ID = db.Column(db.Integer, db.ForeignKey('feedbacktemplate.template_ID'), nullable=False)
+    course_ID = db.Column(db.Integer, db.ForeignKey('course.course_ID'), nullable=False)
+    course_Status = db.Column(db.String(255), nullable=False)
+
+
+    def __init__(self, rcourse_ID, run_Startdate, run_Enddate, run_Starttime, run_Endtime, instructor_ID,
+                 course_Format, course_Venue, runcourse_Status, course_Size, course_Minsize, course_Fee,
+                 class_Duration, reg_Startdate, reg_Enddate, reg_Starttime, reg_Endtime, template_ID,
+                  course_ID, course_Status ):
+        self.rcourse_ID = rcourse_ID
+        self.run_Startdate = run_Startdate
+        self.run_Enddate = run_Enddate
+        self.run_Starttime = run_Starttime
+        self.run_Endtime = run_Endtime
+        self.instructor_ID = instructor_ID
+        self.course_Format = course_Format
+        self.course_Venue = course_Venue
+        self.runcourse_Status = runcourse_Status
+        self.course_Size = course_Size
+        self.course_Minsize = course_Minsize
+        self.course_Fee = course_Fee
+        self.class_Duration = class_Duration
+        self.reg_Startdate = reg_Startdate
+        self.reg_Enddate = reg_Enddate
+        self.reg_Starttime = reg_Starttime
+        self.reg_Endtime = reg_Endtime
+        self.template_ID = template_ID
+        self.course_ID = course_ID
+        self.course_Status =  course_Status    
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+##################  Registration Class Creation ##################
+class Registration(db.Model):
+    __tablename__ = 'registration'
+
+    reg_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    rcourse_ID = db.Column(db.Integer, db.ForeignKey('runcourse.rcourse_ID'),  nullable=False)
+    user_ID = db.Column(db.Integer, db.ForeignKey('user.user_ID'), nullable=False)
+    reg_Status = db.Column(db.String(20), nullable=False) 
+
+
+
+    def __init__(self, reg_ID, rcourse_ID, user_ID, reg_Status):
+        self.reg_ID = reg_ID
+        self.rcourse_ID = rcourse_ID
+        self.user_ID = user_ID
+        self.reg_Status = reg_Status
+
+
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+##################  Lesson Class Creation ##################
+class Lesson(db.Model):
+    __tablename__ = 'lesson'
+
+    lesson_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    reg_ID = db.Column(db.Integer, db.ForeignKey('registration.reg_ID'),  nullable=False)
+    lesson_Date = db.Column(db.Date, nullable=False)
+    lesson_Time = db.Column(db.Time, nullable=False) 
+
+
+
+    def __init__(self, lesson_ID, reg_ID, lesson_Date, lesson_Time):
+        self.lesson_ID = lesson_ID
+        self.reg_ID = reg_ID
+        self.lesson_Date = lesson_Date
+        self.lesson_Time = lesson_Time
+
+
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+##################  Attendence Record Class Creation ##################
+class AttendenceRecord(db.Model):
+    __tablename__ = 'attendancerecord'
+
+    attrecord_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    lesson_ID = db.Column(db.Integer, db.ForeignKey('lesson.lesson_ID'),  nullable=False)
+    status = db.Column(db.String(15), nullable=False)
+    reason = db.Column(db.String(255), nullable=False)
+    attrecord_Status = db.Column(db.String(20), nullable=False) 
+
+
+
+    def __init__(self, attrecord_ID, lesson_ID, status, reason, attrecord_Status):
+        self.attrecord_ID = attrecord_ID
+        self.lesson_ID = lesson_ID
+        self.status = status
+        self.reason = reason
+        self.attrecord_Status = attrecord_Status
+
+
+
+
+    def json(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+##################  Blacklist Class Creation ##################
+class Blacklist(db.Model):
+    __tablename__ = 'blacklist'
+
+    blacklist_ID = db.Column(db.Integer, nullable=False, primary_key=True)
+    user_ID = db.Column(db.Integer, db.ForeignKey('user.user_ID'),  nullable=False)
+    
+
+
+    def __init__(self, blacklist_ID, user_ID ):
+        self.blacklist_ID = blacklist_ID
+        self.user_ID = user_ID
+        
+
+
 
     def json(self):
         columns = self.__mapper__.column_attrs.keys()
