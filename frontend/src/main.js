a8ce711661f4327c1589bbe92aff515d5e9f1116
zhiwei <limzhiwei08@gmail.com>
2023-08-15 17:54:06 +0800

    frontend - created navbar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fas, faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
 // import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import "./assets/style.css" // css
 
-library.add(fas, fab)
+library.add(fas, fab, faEye, faEyeSlash)
 
 createApp(App)
     .use(router)
