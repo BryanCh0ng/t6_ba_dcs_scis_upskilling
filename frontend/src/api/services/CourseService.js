4c1b3e951678f3f799e7129f5bcbe2c491be41c1
zhiwei <limzhiwei08@gmail.com>
2023-08-26 19:18:25 +0800

    search api

--- frontend/src/api/services/CourseService.js
+++ frontend/src/api/services/CourseService.js
@@ -30,6 +30,146 @@ class CourseService extends BaseApiService {
             return this.handleError(error);
         }
     }
+    // Student - Registration - course name, course cat, status
+    async searchCourseRegistrationInfo(queryParams) {
+        try {
+            let registrationInfo = await axiosClient.get("/course/get_course_registration_info_filter_search", { params: queryParams });
+            return registrationInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+    // Student - Vote - course name, course cat, status
+    async searchCourseVoteInfo(queryParams) {
+        try {
+            let voteInfo = await axiosClient.get("/course/get_course_vote_info_filter_search", { params: queryParams });
+            return voteInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+    // Student/Instructor/Trainer - Proposed - course name, course cat, status
+    async searchProposedInfo(queryParams) {
+        try {
+            let proposedInfo = await axiosClient.get("/course/get_proposed_courses_filter_search", { params: queryParams });
+            return proposedInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+    // Student - Completed - course name, course cat
+    async searchCompletedInfo(queryParams) {
+        try {
+            let completedInfo = await axiosClient.get("/course/get_completed_courses_filter_search", { params: queryParams });
+            return completedInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    // Instructor/Trainer - VotingCampaign - course name, course cat
+    async searchVotingCampaignInfo(queryParams) {
+        try {
+            let votingInfo = await axiosClient.get("/course/get_voting_campaign_courses_filter_search", { params: queryParams });
+            return votingInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    // Instructor/Trainer - Assigned Course
+    async searchInstructorAssignedCourseInfo(queryParams) {
+        try {
+            let instructorAssignedInfo = await axiosClient.get("/course/get_instructor_assigned_courses_filter_search", { params: queryParams });
+            return instructorAssignedInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    // Instructor/Trainer - Assigned Course
+    async searchInstructorProposedCourseInfo(queryParams) {
+        try {
+            let instructorProposedInfo = await axiosClient.get("/course/get_instructor_proposed_courses_filter_search", { params: queryParams });
+            return instructorProposedInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    // Instructor/Trainer - Completed/Taught Course
+    async searchInstructorCompletedCourseInfo(queryParams) {
+        try {
+            let instructorCompletedInfo = await axiosClient.get("/course/get_instructor_taught_courses_filter_search", { params: queryParams });
+            return instructorCompletedInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    // Admin - All Proposal - Submitted
+    async searchAdminAllProposalInfo(queryParams) {
+        try {
+            let adminAllProposalInfo = await axiosClient.get("/course/get_all_submitted_proposed_courses_admin", { params: queryParams });
+            return adminAllProposalInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    // Admin - All Proposal - Approved/Rejected
+    async searchAdminAllApprovedRejectedProposalInfo(queryParams) {
+        try {
+            let adminAllApprovedRejectedProposalInfo = await axiosClient.get("/course/get_all_app_reg_proposed_courses_admin", { params: queryParams });
+            return adminAllApprovedRejectedProposalInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    // Admin - All Voting Campaign
+    async searchAdminVotingCampaignInfo(queryParams) {
+        try {
+            let adminVotingCampaignInfo = await axiosClient.get("/course/get_all_voting_courses_admin", { params: queryParams });
+            return adminVotingCampaignInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    // Admin - All Courses
+    async searchAdminAllCoursesInfo(queryParams) {
+        try {
+            let adminAllCoursesInfo = await axiosClient.get("/course/get_all_courses_with_registration_count", { params: queryParams });
+            return adminAllCoursesInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
+    // Admin - All Instructors
+    async searchAdminAllInstructorsInfo(queryParams) {
+        try {
+            let adminAllInstructorsInfo = await axiosClient.get("/course/get_all_instructors_and_trainers", { params: queryParams });
+            return adminAllInstructorsInfo.data;
+
+        } catch (error) {
+            return this.handleError(error);
+        }
+    }
+
 }
 
 export default new CourseService();
