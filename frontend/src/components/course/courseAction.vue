67350ae4b37e4c5cfafefad45169414a5196dafa
yyiiinn <33303522+yyiiinn@users.noreply.github.com>
2023-08-30 02:46:27 +0800

    merge conflict;

--- frontend/src/components/course/courseAction.vue
+++ frontend/src/components/course/courseAction.vue
@@ -6,7 +6,6 @@
     <button @click="runCourseAction(course.course_ID)" class="btn btn-deactivate deactivate text-light font-weight-bold text-nowrap" v-else-if="status == 'Deactivate'">Deactivate</button>
     <button @click="runCourseAction(course.course_ID)" class="w-100 btn btn-activate activate text-light font-weight-bold text-nowrap" v-else-if="status == 'Activate'">Activate</button>
     <button @click=runCourseAction(course.course_ID) class="btn btn-blue-green hop-on text-light font-weight-bold text-nowrap" v-if="status == 'Active'">Hop On</button>
-    <button class="btn btn-edit edit text-light font-weight-bold text-nowrap" v-else-if="status == 'Edit'">Edit</button>
     <button class="btn btn-activate approve text-light font-weight-bold text-nowrap" v-else-if="status == 'pending_approve'">Approve</button>
     <button class="btn btn-danger reject text-light font-weight-bold text-nowrap" v-else-if="status == 'pending_reject'">Reject</button>
     <button class="btn btn-info open_for_voting text-light font-weight-bold text-nowrap" v-else-if="status == 'Approved'">Open for Voting</button>
@@ -47,11 +46,15 @@ export default {
           response = await CourseService.activateRunCourse(this.course.course_ID);
         } else if (this.status == 'Deactivate') {
           response = await CourseService.deactivateRunCourse(this.course.course_ID);
+        } else  if (this.status == 'Active') {
+          this.get_user_id();
+          response = await RegistrationService.createNewRegistration(this.course.rcourse_ID, 1, "Pending");
         }
         this.message = response.message;
         this.$emit('action-and-message-updated', {message: this.message, course: this.course});
       } catch (error) {
         this.message = error.message
+        this.$emit('action-and-message-updated', {message: this.message, course: this.course});
       }
     },
       async get_user_id() {
@@ -63,23 +66,7 @@ export default {
           this.message = error.message
           this.user_ID = null;
         }
-      },
-    async runCourseAction() {
-      try {
-        // let response;
-        if (this.status == 'Active') {
-          // response = await regservice
-          this.get_user_id();
-          const response = await RegistrationService.createNewRegistration(this.course.rcourse_ID, 1, "Pending");
-          this.message = response.message;
-        }
-        this.$emit('action-and-message-updated', {message: this.message, course: this.course});
-      } catch (error) {
-        console.log(error);
-        this.message = error.message;
-        this.$emit('action-and-message-updated', {message: this.message, course: this.course});
       }
-    }
   }
 };
 </script>
