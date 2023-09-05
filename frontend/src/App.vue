d49c62eb55a74ce823e9553e8ccc22cadccd6166
yimunnn <yimun.yip.2020@scis.smu.edu.sg>
2023-09-06 00:52:19 +0800

    Added navbar to the page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,7 +20,7 @@ export default {
   computed: {
     showNavBar() {
       // Specify the routes where you want to show the navbar
-      const routesWithNavBar = ["ContactUs", "viewCourse","adminViewRunCourse", "adminViewVoteCourse", "adminViewProposedCourse","adminVotingCampaign", "adminViewInstructorsTrainers", "studentViewCourse"];
+      const routesWithNavBar = ["ContactUs", "viewCourse","adminViewRunCourse", "adminViewVoteCourse", "adminViewProposedCourse","adminVotingCampaign", "adminViewInstructorsTrainers", "studentViewCourse", "studentProposeCourse", "instructorTrainerProposeCourse"];
       return routesWithNavBar.includes(this.$route.name);
     }
   }
