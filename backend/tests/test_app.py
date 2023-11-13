1dda2911f8fcafcb157602940958917466bc766d
nicwong27 <65439133+nicwong27@users.noreply.github.com>
2023-11-13 19:39:45 +0800

    tests calling a method

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,7 +1,6 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from flask import Flask, json
-from werkzeug.test import EnvironBuilder
 
 import os
 import sys
@@ -10,58 +9,31 @@ sys.path.append(parent_dir)
 
 from core_features.course import *
 
-class TestCreateCourseEndpoint(unittest.TestCase):
+class TestCourse(unittest.TestCase):
     def setUp(self):
         # Create a Flask test client
         self.app = Flask(__name__)
         self.app.testing = True
         self.client = self.app.test_client()
 
-    def _create_request(self, json_data):
-        # Helper method to create a Flask request context
-        builder = EnvironBuilder(method='POST', data=json.dumps(json_data))
-        return self.app.request_context(builder.get_environ())
-
-    @patch('core_features.course.db.session.commit')
-    @patch('core_features.course.jsonify')
-    @patch('core_features.course.Course.query', autospec=True)  # Ensure that Course.query is treated as a real object
-    def test_create_course_success(self, mock_query, mock_jsonify, mock_commit):
-        # Arrange
-        with self._create_request({"course_Name": "Test Course", "course_Desc": "Description", "coursecat_ID": 1}):
-            mock_query.return_value.filter.return_value.first.return_value = None
-            mock_commit.return_value = None
-
+    def test_addition(self):
+        with self.app.test_request_context(json={"first": 5, "second": 10}):
             # Act
-            response = CreateCourse().post()
+            response = Addition().post()
 
-        # Assert
-        mock_query.assert_called_once()  # Ensure that Course.query is accessed
-        mock_query.return_value.filter.assert_called_once()
-        mock_commit.assert_called_once()
-        mock_jsonify.assert_called_once()
-
-        # Add more assertions based on the expected behavior of your endpoint
-        self.assertEqual(response.status_code, 201)
-        self.assertIn(b'Course created successfully', response.data)
-
-    @patch('core_features.course.Course.query', autospec=True)  # Ensure that Course.query is treated as a real object
-    def test_create_course_failure_existing_course(self, mock_query):
+            # Assert
+            self.assertEqual(response, 15)
+    
+    def test_get_all_courses_filter_search(self):
         # Arrange
-        with self._create_request({"course_Name": "Existing Course", "course_Desc": "Description", "coursecat_ID": 1}):
-            existing_course = MagicMock()
-            mock_query.return_value.filter.return_value.first.return_value = existing_course
-
+        with self.app.test_request_context("/retrieve_all_courses_filter_search"):
             # Act
-            response = CreateCourse().post()
+            response = GetAllCoursesFilterSearch().get()
 
-        # Assert
-        mock_query.assert_called_once()  # Ensure that Course.query is accessed
-        mock_query.return_value.filter.assert_called_once()
-        existing_course.json.assert_called_once()
+            # Assert
+            self.assertEqual(response.status_code, 200)
+            # Add more assertions based on the expected behavior of your endpoint
 
-        # Add more assertions based on the expected behavior of your endpoint
-        self.assertEqual(response.status_code, 409)
-        self.assertIn(b'Course name already exists', response.data)
 
 if __name__ == '__main__':
     unittest.main()
